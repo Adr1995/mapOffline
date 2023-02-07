45da6c10690b4a1d1869339ea9129f3ef01a9e63
Adr1995 <66148847+Adr1995@users.noreply.github.com>
2023-02-07 23:46:56 +0000

    Añadido nuevo mapa satelite

--- map.js
+++ map.js
@@ -1,8 +1,15 @@
 "use strict";
 var L = window['L'];
 var map = L.map('map').setView([27.955280, -15.597910], 13);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+var satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+attribution: 'Tiles © Esri — Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+satellite.addTo(map);
 var marker = L.marker([27.955280, -15.597910]).addTo(map);
 marker.bindPopup('A pretty CSS3 popup.<br> Easily customizable.').openPopup();
+var control = L.Routing.control({
+  waypoints: [
+    L.latLng(57.74, 11.94),
+    L.latLng(57.6792, 11.949)
+  ]
+}).addTo(map);
